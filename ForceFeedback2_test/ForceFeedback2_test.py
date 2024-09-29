fae1bdd6a68b2261f86722964ce075496c45e718
Keith Baston <keith.baston@gmail.com>
2024-09-28 22:45:43 -0700

    Adding the python side.

--- ForceFeedback2_test/ForceFeedback2_test.py
+++ ForceFeedback2_test/ForceFeedback2_test.py
@@ -23,26 +23,57 @@ pov_idx = [0, 4500, 9000, 13500, 18000, 22500, 27000, 31500]
 
 Settings = collections.namedtuple("Settings", ["sensitivity"])
 
+THROTTLE_DEAD_START = int(0x4000 * 0.95)
+THROTTLE_DEAD_STOP = int(0x4000 * 1.05)
+
 
 async def joy_poller(settings):
     vjoy = pyvjoy.VJoyDevice(1)
     vjoy.reset()
+
+    old_val = 0 
+
+    buzzer = PyForceFeedback2.BuzzForce()
+
+    throttle_dead = False
     while True:
         await asyncio.sleep(0) # Yeild
         joy_state = PyForceFeedback2.poll()
 
+        layer = joy_state.buttons[5]
+        buttons = list(joy_state.buttons)
+        del(buttons[5])
+        start = 0
+        if layer:
+            start= 15
+
         but_val = 0
-        for idx, but_state in enumerate(joy_state.buttons):
+        for idx, but_state in enumerate(buttons, start = start ):
             if but_state:
                 but_val += 1 << idx
 
         vjoy.data.wAxisX = map_axis(joy_state.x)
         vjoy.data.wAxisY = map_axis(joy_state.y)
         vjoy.data.wAxisZRot = map_axis(joy_state.r_z)
-        vjoy.data.wAxisZ = map_axis(joy_state.throttle)
+
+        throttle = map_axis(joy_state.throttle)
+        if old_val != throttle:
+            print(throttle)
+            old_val = throttle
+     
+        if THROTTLE_DEAD_START < throttle < THROTTLE_DEAD_STOP:
+            vjoy.data.wAxisZ = 0x4000
+            if not throttle_dead:
+                print("buzz")
+                buzzer.start()
+                throttle_dead = True
+        else:
+            
+            vjoy.data.wAxisZ = map_axis(joy_state.throttle)
+            throttle_dead = False
         
         if joy_state.pov is not None:
-            but_val += 1 << (pov_idx.index(joy_state.pov) + 8)
+            but_val += 1 << (pov_idx.index(joy_state.pov) + (7 + start))
                 
         vjoy.data.lButtons = but_val
 
@@ -51,14 +82,16 @@ async def joy_poller(settings):
 async def force_feed_back(settings):
     PyForceFeedback2.acquire()
     x_y_force = PyForceFeedback2.ConstantForce()
-    
+
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None,x_y_force.set_direction, 0, 0 )
     
 
     svr = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     svr.bind(("127.0.0.1", 10001))	
     svr.setblocking(False)
 
-    loop = asyncio.get_running_loop()
+    
     
     min_val = 100
     max_val = 0
@@ -84,8 +117,8 @@ async def force_feed_back(settings):
         if vert_g < min_val:
             min_val = vert_g
 
-        lat_dir = max( -10000, min(10000, int(math.tanh(late_g/1.6) * 10000)))
-        long_dir = max( -10000, min(10000, int(math.tanh(long_g/1.6) * 10000)))
+        lat_dir = max( -10000, min(10000, int(math.tanh(late_g) * 10000)))
+        long_dir = max( -10000, min(10000, int(math.tanh(long_g) * 10000)))
              
         await loop.run_in_executor(None,x_y_force.set_direction, lat_dir, long_dir )
          
@@ -104,6 +137,7 @@ if __name__ == "__main__":
     try:
         print("init")
         PyForceFeedback2.init()
+        PyForceFeedback2.acquire()
         asyncio.run(main())
         print("inited")
     finally:
---
+++ ForceFeedback2_test/profile_test.py
@@ -0,0 +1,12 @@
+import cProfile
+import io 
+import pstats
+import PyForceFeedback2
+
+PyForceFeedback2.init()
+PyForceFeedback2.acquire()
+buzz = PyForceFeedback2.BuzzForce()
+#buzz.start()
+
+
+
